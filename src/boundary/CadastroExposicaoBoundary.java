b77f6c49c4d1b13442d69d577f60422d94d93a57
William de Araujo <estudoswill@gmail.com>
2016-06-22 21:40:07 -0300

    up

--- src/boundary/CadastroExposicaoBoundary.java
+++ src/boundary/CadastroExposicaoBoundary.java
@@ -3,16 +3,31 @@ package boundary;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Component;
+import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.JComponent;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
-public class CadastroExposicaoBoundary {
-	JPanel panelExposicao;
-	JDialog CadDialog = new JDialog();
+import controller.ExposicaoController;
+
+public class CadastroExposicaoBoundary implements ActionListener, MouseListener, ListSelectionListener{
+	private JPanel panelExposicao;
+	private JDialog CadDialog = new JDialog();
+	private ExposicaoController control = new ExposicaoController();
+	private JTable tabelaExposicao;
+	private JScrollPane panTableExposicao;
 	public CadastroExposicaoBoundary() {
 	
 		panelExposicao = new JPanel(new BorderLayout());
@@ -45,8 +60,15 @@ public class CadastroExposicaoBoundary {
 	
 	
 	public JComponent Centro() {
-		JPanel JPaneCentro = new JPanel();
+		JPanel JPaneCentro = new JPanel(new GridLayout(1,1));
+		panTableExposicao = new JScrollPane();
+		tabelaExposicao = new JTable(control);
+		panTableExposicao.getViewport().add(tabelaExposicao);
+		tabelaExposicao.getSelectionModel().addListSelectionListener(this);
+		tabelaExposicao.addMouseListener(this);
 		
+		
+		JPaneCentro.add(panTableExposicao);
 		
 		
 		
@@ -58,6 +80,48 @@ public class CadastroExposicaoBoundary {
 		
 		
 		return botoes;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void valueChanged(ListSelectionEvent arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
