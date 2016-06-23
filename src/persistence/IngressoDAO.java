810a40b9bd9c61c042d84ee195ff4bbc9a8057a7
willasgard <estudoswill@gmail.com>
2016-06-23 12:13:54 -0300

    Ajustando Tabela de Vendas

--- src/persistence/IngressoDAO.java
+++ src/persistence/IngressoDAO.java
@@ -2,7 +2,10 @@ package persistence;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import entity.Ingresso;
 
@@ -21,21 +24,21 @@ Connection con;
 	
 	@Override
 	public void InsereVenda(Ingresso venda) {
-		String sql = "INSERT INTO `venda` (id ,Exposicao , IdadeMenor5 , IdadeMaior60, Estudante, Professor, Outros, ValorInt, Desconto, Total)"
-				+ "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+		String sql = "INSERT INTO `venda` (Exposicao , IdadeMenor5 , IdadeMaior60, Estudante, Professor, Outros, ValorInt, Desconto, Total)"
+				+ "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
 		try{
 		PreparedStatement ps = con.prepareStatement(sql);
 		
-		ps.setInt(1, venda.getId());
-		ps.setString(2, venda.getObrasexp());
-		ps.setBoolean(3,venda.isIdade5());
-		ps.setBoolean(4, venda.isIdade60());
-		ps.setBoolean(5,venda.isEstudante());
-		ps.setBoolean(6, venda.isProfessor());
-		ps.setBoolean(7, venda.isOutros());
-		ps.setDouble(8, venda.getValor());
-		ps.setDouble(9, venda.getDesconto());
-		ps.setDouble(10, venda.getTotal());
+		
+		ps.setString(1, venda.getObrasexp());
+		ps.setBoolean(2,venda.isIdade5());
+		ps.setBoolean(3, venda.isIdade60());
+		ps.setBoolean(4,venda.isEstudante());
+		ps.setBoolean(5, venda.isProfessor());
+		ps.setBoolean(6, venda.isOutros());
+		ps.setDouble(7, venda.getValor());
+		ps.setDouble(8, venda.getDesconto());
+		ps.setDouble(9, venda.getTotal());
 		
 		ps.execute();
 		ps.close();
@@ -44,7 +47,22 @@ Connection con;
 		}		
 	}
 	
-	public void ListaIngressoVendidos (Ingresso vendas){
+	public List<Ingresso> ListaIngressoVendidos () throws SQLException{
+		List<Ingresso> listaVendidos = new ArrayList<Ingresso>();
+		String sql = "SELECT * FROM venda";
+		PreparedStatement ps = con.prepareStatement(sql);
+		ResultSet rs = ps.executeQuery();
+		while (rs.next()){
+			Ingresso vendas = new Ingresso();
+			vendas.setId(rs.getInt("id"));
+			vendas.setObrasexp(rs.getString("Exposicao"));
+			vendas.setValor(rs.getDouble("ValorInt"));
+			vendas.setDesconto(rs.getDouble("Desconto"));
+			vendas.setTotal(rs.getDouble("Total"));
+			listaVendidos.add(vendas);
+		}
+		
+		return listaVendidos;
 		
 	}
 	
