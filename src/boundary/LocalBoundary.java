47c88a9db89324ce1106d3e4ad6bec31aa7b1f31
Erick e Paula <Erick e Paula@Erick>
2016-06-22 21:45:58 -0300

    teste

--- src/boundary/LocalBoundary.java
+++ src/boundary/LocalBoundary.java
@@ -62,6 +62,7 @@ public class LocalBoundary extends JFrame implements ActionListener {
 		LocalDialog.setSize(720, 400);
 		LocalDialog.setLocationRelativeTo(null);
 		LocalDialog.setVisible(true);
+		System.out.println("Teste");
 
 	}
 
