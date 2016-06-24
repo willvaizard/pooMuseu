c871a5325eaf2e1ac09d037fd4d35e0b72611508
tecnico <tecnico@10.68.76.182>
2016-06-24 20:37:32 -0300

    ok

--- src/persistence/JDBCUtil.java
+++ src/persistence/JDBCUtil.java
@@ -38,7 +38,6 @@ public class JDBCUtil {
 			try {
 				con = DriverManager.getConnection(JDBC_URL,JDBC_USER, JDBC_PASSWORD);
 			} catch (SQLException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
@@ -54,34 +53,4 @@ public class JDBCUtil {
 		con = null;
 	}
 
-	
-
-	
-	/*
-	
-	private static Driver driver = null;
-
-	public static synchronized Connection getConnection() throws SQLException {
-		if (driver == null) {
-			try {
-				Class jdbcDriverClass = Class.forName(JDBC_DRIVER);
-				driver = (Driver) jdbcDriverClass.newInstance();
-				DriverManager.registerDriver(driver);
-			} catch (Exception e) {
-				System.out.println("Failed to initialise JDBC driver");
-				e.printStackTrace();
-			}
-		}
-		return DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASSWORD);
-	}
-
-	public static void close(Connection conn) {
-		try {
-			if (conn != null)
-				conn.close();
-		} catch (SQLException sqle) {
-			sqle.printStackTrace();
-		}
-	}
-*/
 }
